1349b2b365dde5fed5796792d4065441e9d2e4b2
Alexander Babak <bbkshow1987@gmail.com>
2022-10-30 19:58:48 +0200

    add app

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,43 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import { HomePage } from "modules/dashboard/HomePage";
+import { LoginPage } from "./modules/auth/login/LoginPage";
+import { RegisterPage } from "modules/auth/registration/RegisterPage";
+import { RegisterEmailPage } from "modules/auth/registration/RegisterEmailPage";
+import { RegisterCompletePage } from "modules/auth/registration/RegisterCompletePage";
+import { ResetPage } from "modules/auth/resetPassword/ResetPage";
+import { ResetEmailPage } from "modules/auth/resetPassword/ResetEmailPage";
+import { ResetCompletePage } from "modules/auth/resetPassword/ResetCompletePage";
+import { NotFoundPage } from "modules/NotFoundPage";
+import { TermsConditionsPage } from "modules/auth/registration/TermsConditionsPage";
+import { AuthProvider, UserProvider } from "routes/AuthProvider";
+import { RegisterLayout } from "components/layouts/RegisterLayout";
+import { ResetLayout } from "components/layouts/ResetLayout";
 import "assets/styles/App.css";
 
 export function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route element={<UserProvider />}>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterLayout />}>
+            <Route index element={<RegisterPage />} />
+            <Route path="email-sent" element={<RegisterEmailPage />} />
+            <Route path="complete" element={<RegisterCompletePage />} />
+          </Route>
+          <Route path="/reset" element={<ResetLayout />}>
+            <Route index element={<ResetPage />} />
+            <Route path="email-sent" element={<ResetEmailPage />} />
+            <Route path="complete" element={<ResetCompletePage />} />
+          </Route>
+          <Route path="/terms" element={<TermsConditionsPage />} />
+        </Route>
+        <Route element={<AuthProvider />}>
+          <Route path="/" element={<HomePage />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "assets/styles/index.css";
-import { App } from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "redux/store";
+import { App } from "App";
 import reportWebVitals from "./reportWebVitals";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "assets/styles/theme";
+import "./firebase";
+import "assets/styles/index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
