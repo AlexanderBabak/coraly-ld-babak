99d754cdaadbb92ba7ee0ab8e9dd6ebed4a3502d
Alexander Babak <bbkshow1987@gmail.com>
2022-10-31 10:43:11 +0200

    fix reset page

--- src/modules/auth/resetPassword/ResetCompletePage.tsx
+++ src/modules/auth/resetPassword/ResetCompletePage.tsx
@@ -1,21 +1,6 @@
-import { ResetParams } from "api/auth/authDto";
-import { useForm } from "react-hook-form";
 import { ResetCompleteForm } from "./components/ResetCompleteForm";
 
 export const ResetCompletePage = () => {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm<ResetParams>();
-
   const onSubmit = () => {};
-  return (
-    <ResetCompleteForm
-      handleSubmit={handleSubmit}
-      onSubmit={onSubmit}
-      errors={errors}
-      register={register}
-    />
-  );
+  return <ResetCompleteForm onSubmit={onSubmit} />;
 };
--- src/modules/auth/resetPassword/ResetPage.tsx
+++ src/modules/auth/resetPassword/ResetPage.tsx
@@ -1,21 +1,6 @@
-import { LoginParams } from "api/auth/authDto";
-import { useForm } from "react-hook-form";
 import { ResetForm } from "./components/ResetForm";
 
 export const ResetPage = () => {
-  const {
-    register,
-    formState: { errors },
-    handleSubmit,
-  } = useForm<LoginParams>();
-
   const onSubmit = () => {};
-  return (
-    <ResetForm
-      handleSubmit={handleSubmit}
-      onSubmit={onSubmit}
-      errors={errors}
-      register={register}
-    />
-  );
+  return <ResetForm onSubmit={onSubmit} />;
 };
