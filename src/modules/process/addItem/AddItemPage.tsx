caaaefc0ba94098c097bb992d3614320ad880eda
Alexander Babak <bbkshow1987@gmail.com>
2022-11-06 22:42:20 +0200

    add left side AddItemPage

--- src/modules/process/addItem/AddItemPage.tsx
+++ src/modules/process/addItem/AddItemPage.tsx
@@ -1,17 +1,24 @@
 import React from "react";
-import { StyledButton, StyledTypography } from "components/reusable";
+import {
+  StackStyled,
+  StyledButton,
+  StyledTypography,
+} from "components/reusable";
 import {
   IconButton,
   Stack,
   Box,
   Modal,
-  AvatarGroup,
-  Avatar,
   Link,
   useTheme,
+  Chip,
+  Divider,
+  FormControl,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
 } from "@mui/material";
 import { InputCards } from "components/reusable/InputCards";
-import { InputIcon } from "components/customInputs/InputIcon";
 
 import FolderOutlinedIcon from "@mui/icons-material/FolderOutlined";
 import CenterFocusWeakOutlinedIcon from "@mui/icons-material/CenterFocusWeakOutlined";
@@ -21,6 +28,14 @@ import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlined";
 import CloseOutlinedIcon from "@mui/icons-material/CloseOutlined";
 import RemoveRedEyeOutlinedIcon from "@mui/icons-material/RemoveRedEyeOutlined";
 import { DividerVertical } from "../processItem/toolbar/DividerVertical";
+import { AvatarsGroup } from "./AvatarsGroup";
+import { ButtonAdd } from "components/buttonAdd/ButtonAdd";
+import { CreatorSVG } from "components/сreatorSVG/CreatorSVG";
+import ExpandMoreOutlinedIcon from "@mui/icons-material/ExpandMoreOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import PropaneTankOutlinedIcon from "@mui/icons-material/PropaneTankOutlined";
+import DescriptionOutlinedIcon from "@mui/icons-material/DescriptionOutlined";
 
 type Props = {
   openModal: boolean;
@@ -32,6 +47,7 @@ export const AddItemPage: React.FC<Props> = ({
   handleCloseModal,
 }) => {
   const { palette } = useTheme();
+
   const styles = {
     content: {
       position: "absolute",
@@ -39,17 +55,15 @@ export const AddItemPage: React.FC<Props> = ({
       left: "50%",
       transform: "translate(-50%, -50%)",
       // здесь width
-      minWidth: 1200,
+      // minWidth: 1200,
+      width: "90%",
+      // height: "90vh",
       bgcolor: "background.paper",
       borderRadius: "15px",
-      p: 3,
-    },
-    avatar: {
-      width: 24,
-      height: 24,
-      backgroundColor: palette.info.main,
-      fontSize: 8,
-      marginRight: 1,
+      p: 4,
+      justifyContent: "space-between",
+      display: "flex",
+      flexDirection: "column",
     },
   };
   return (
@@ -60,6 +74,7 @@ export const AddItemPage: React.FC<Props> = ({
       aria-describedby="modal-modal-description"
     >
       <Box sx={styles.content}>
+        {/* header */}
         <Stack direction="row" justifyContent="space-between">
           <StyledTypography fontSize={20} fontWeight={700} lineHeight="36px">
             Courtney@mail.com
@@ -88,6 +103,7 @@ export const AddItemPage: React.FC<Props> = ({
                 <DeleteOutlinedIcon />
               </IconButton>
             </Stack>
+
             <Stack direction="row">
               <DividerVertical />
               <IconButton onClick={handleCloseModal}>
@@ -96,19 +112,318 @@ export const AddItemPage: React.FC<Props> = ({
             </Stack>
           </Stack>
         </Stack>
+        {/* main */}
+        <Stack direction="row" justifyContent="space-between" marginTop={1}>
+          {/* убрать потом ширину */}
+          {/* left main */}
+          <Stack spacing={1} width="50%" paddingRight={2}>
+            <StackStyled justifyContent="space-between">
+              <StackStyled>
+                <AvatarsGroup />
+                <ButtonAdd />
+              </StackStyled>
 
-        <Stack>
-          <Stack direction="row">
-            <AvatarGroup max={4}>
-              <Avatar sx={styles.avatar}>PL</Avatar>
-              <Avatar>PL</Avatar>
-              <Avatar>PL</Avatar>
-            </AvatarGroup>
-            <StyledTypography title="Hello"></StyledTypography>
+              <StackStyled>
+                <IconButton
+                  disableRipple
+                  size="small"
+                  sx={{ marginRight: "4px" }}
+                >
+                  <CreatorSVG
+                    iconName={"MacroFasi"}
+                    color={palette.primary.main}
+                  />
+                </IconButton>
+
+                <StyledTypography
+                  fontWeight={600}
+                  lineHeight="18px"
+                  color="#6F6D7B"
+                >
+                  Chapter One
+                </StyledTypography>
+                <IconButton disableRipple size="small">
+                  <ExpandMoreOutlinedIcon color="disabled" />
+                </IconButton>
+              </StackStyled>
+
+              <StackStyled columnGap={1}>
+                <CalendarTodayOutlinedIcon sx={{ color: "#6F6D7B" }} />
+                <StyledTypography
+                  fontWeight={600}
+                  lineHeight="18px"
+                  color="#6F6D7B"
+                >
+                  05/10/2021 - 17:50
+                </StyledTypography>
+              </StackStyled>
+            </StackStyled>
+
+            <StackStyled direction="row" spacing={1}>
+              <Chip
+                label="Label 1"
+                sx={{
+                  bgcolor: "#47BDFF",
+                  color: "#fff",
+                  width: 82,
+                  height: 22,
+                  fontSize: 14,
+                  fontWeight: 600,
+                  lineHeight: 18,
+                }}
+              />
+              <Chip
+                label="Label 2"
+                sx={{
+                  bgcolor: "#E547FF",
+                  color: "#fff",
+                  width: 82,
+                  height: 22,
+                  fontSize: 14,
+                  fontWeight: 600,
+                  lineHeight: 18,
+                }}
+              />
+              <Chip
+                label="Label 3"
+                sx={{
+                  bgcolor: "#FF9F47",
+                  color: "#fff",
+                  width: 82,
+                  height: 22,
+                  fontSize: 14,
+                  fontWeight: 600,
+                  lineHeight: 18,
+                }}
+              />
+              <ButtonAdd />
+            </StackStyled>
+
+            <StackStyled direction="row" justifyContent="space-between">
+              <StackStyled direction="row" alignItems="center">
+                <PersonOutlineOutlinedIcon
+                  sx={{ color: "#6F6D7B", marginRight: 1 }}
+                />
+                <StyledTypography
+                  fontWeight={600}
+                  lineHeight="18px"
+                  color="#6F6D7B"
+                >
+                  Select vendors
+                </StyledTypography>
+                <IconButton disableRipple size="small">
+                  <ExpandMoreOutlinedIcon color="disabled" />
+                </IconButton>
+              </StackStyled>
+
+              <DividerVertical />
+
+              <StackStyled columnGap={1}>
+                <Chip
+                  icon={<PropaneTankOutlinedIcon fontSize="small" />}
+                  label="#database_object_1"
+                  sx={{
+                    bgcolor: "#D6D5D9",
+                    color: "#312E43",
+
+                    height: 22,
+                    fontSize: 14,
+                    fontWeight: 400,
+                    lineHeight: 18,
+                    padding: "0 10px",
+                  }}
+                />
+                <Chip
+                  icon={<PropaneTankOutlinedIcon fontSize="small" />}
+                  label="#db-oject1"
+                  sx={{
+                    bgcolor: "#D6D5D9",
+                    color: "#312E43",
+
+                    height: 22,
+                    fontSize: 14,
+                    fontWeight: 400,
+                    lineHeight: 18,
+                    padding: "0 10px",
+                  }}
+                />
+              </StackStyled>
+            </StackStyled>
+            <Divider />
+
+            <Stack spacing={2}>
+              <StyledTypography
+                color="#464356"
+                fontWeight={600}
+                lineHeight="24px"
+              >
+                Startform Name
+              </StyledTypography>
+              <InputCards
+                label="Email"
+                size="small"
+                sx={{ paddingBottom: 0 }}
+              />
+              <InputCards
+                label="Description test"
+                size="small"
+                helperText="This is a description"
+                multiline
+                minRows={2}
+                sx={{ paddingBottom: 0 }}
+              />
+            </Stack>
+
+            <Stack spacing={2}>
+              <StyledTypography
+                color="#464356"
+                fontWeight={600}
+                lineHeight="24px"
+              >
+                Company data
+              </StyledTypography>
+              <Stack direction="row" spacing={2}>
+                <Stack alignItems="center" spacing={1}>
+                  <DescriptionOutlinedIcon sx={{ color: "#9897A1" }} />
+                  <Divider
+                    orientation="vertical"
+                    sx={{ borderRightWidth: 2, height: 150 }}
+                  />
+                </Stack>
+
+                <Stack spacing={2} flexGrow={1}>
+                  <InputCards
+                    label="Company name"
+                    size="small"
+                    sx={{ paddingBottom: 0 }}
+                  />
+                  <InputCards
+                    label="Surname"
+                    size="small"
+                    sx={{ paddingBottom: 0 }}
+                  />
+                  <FormControl>
+                    <RadioGroup defaultValue="person">
+                      <FormControlLabel
+                        value="company"
+                        control={<Radio sx={{ padding: "5px" }} />}
+                        label="Company"
+                        sx={{
+                          "& .MuiFormControlLabel-label": {
+                            fontSize: 14,
+                            lineHeight: "18px",
+                          },
+                        }}
+                      />
+                      <FormControlLabel
+                        value="person"
+                        control={<Radio sx={{ padding: "5px" }} />}
+                        label="Person"
+                        sx={{
+                          "& .MuiFormControlLabel-label": {
+                            fontSize: 14,
+                            lineHeight: "18px",
+                          },
+                        }}
+                      />
+                    </RadioGroup>
+                  </FormControl>
+                </Stack>
+              </Stack>
+            </Stack>
+
+            <Stack spacing={2}>
+              <StyledTypography
+                color="#464356"
+                fontWeight={600}
+                lineHeight="24px"
+              >
+                Gender
+              </StyledTypography>
+              <FormControl sx={{ paddingLeft: 1 }}>
+                <RadioGroup defaultValue="female">
+                  <FormControlLabel
+                    value="male"
+                    control={<Radio sx={{ padding: "5px" }} />}
+                    label="Male"
+                    sx={{
+                      "& .MuiFormControlLabel-label": {
+                        fontSize: 14,
+                        lineHeight: "18px",
+                      },
+                    }}
+                  />
+                  <FormControlLabel
+                    value="female"
+                    control={<Radio sx={{ padding: "5px" }} />}
+                    label="Female"
+                    sx={{
+                      "& .MuiFormControlLabel-label": {
+                        fontSize: 14,
+                        lineHeight: "18px",
+                      },
+                    }}
+                  />
+                  <FormControlLabel
+                    value="not declared"
+                    control={<Radio sx={{ padding: "5px" }} />}
+                    label="Not declared"
+                    sx={{
+                      "& .MuiFormControlLabel-label": {
+                        fontSize: 14,
+                        lineHeight: "18px",
+                      },
+                    }}
+                  />
+                </RadioGroup>
+              </FormControl>
+            </Stack>
+          </Stack>
+
+          {/* right main */}
+          <Stack alignItems="center" bgcolor="aqua" width="50%">
+            Right Side
           </Stack>
         </Stack>
 
-        <Stack>{/* right side */}</Stack>
+        {/* footer */}
+        <Stack
+          direction="row"
+          alignItems="center"
+          justifyContent="space-between"
+          marginTop={2}
+        >
+          <StyledTypography
+            color="#464356"
+            fontWeight={600}
+            fontSize={16}
+            lineHeight="24px"
+          >
+            Fase ID:{" "}
+            <Box
+              component="span"
+              sx={{ fontWeight: 400, fontSize: 14, lineHeight: "18px" }}
+            >
+              61571535e7058c00143322b8
+            </Box>
+          </StyledTypography>
+          <Stack flexDirection="row" justifyContent="flex-end">
+            <StyledButton
+              text="Annula"
+              fullWidth={false}
+              variant="outlined"
+              onClick={handleCloseModal}
+            ></StyledButton>
+            <StyledButton
+              text="Crea"
+              fullWidth={false}
+              marginleft="16px"
+              color="secondary"
+              disableElevation={true}
+              onClick={() => console.log("Create Process!")}
+            ></StyledButton>
+          </Stack>
+        </Stack>
       </Box>
     </Modal>
   );
--- src/modules/process/processCommon/modal/ModalPersonProcess.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ModalPersonProcess = () => {
-  return <div></div>;
-};
-
-export default ModalPersonProcess;
